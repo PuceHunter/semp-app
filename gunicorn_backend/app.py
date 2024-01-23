3cd50d82f90977c7b6c97050650fa4e839f68f2d
RikinZala25 <rikinzala.25@gmail.com>
2024-01-23 14:23:50 +0530

    integrate: backend

--- gunicorn_backend/app.py
+++ gunicorn_backend/app.py
@@ -7,12 +7,12 @@ CORS(app)  # Enable CORS for all routes
 # Configure route to /
 @app.route('/')
 def get_data_index_route():
-    return "Hello World, from Rikin Zala! \n Search for `/message` route"
+    return "Hello World, from Rikin Zala!"
 
 # Configure route to /message
 @app.route('/message')
 def get_data_message_route():
-    data = "This is message route"
+    data = "Rikin Zala says, this is the `/message` route"
     response_data = {"message": data}
     return jsonify(response_data)
 
